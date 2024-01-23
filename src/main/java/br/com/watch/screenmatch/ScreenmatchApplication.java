884432c1e8b3583e3c7222188d35e85f306efb0f
Felipe Gabriel <felipegabrielodss@gmail.com>
2024-01-23 14:18:07 -0300

    Modelando dados da série

--- src/main/java/br/com/watch/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/watch/screenmatch/ScreenmatchApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.watch.screenmatch.model.DadosSerie;
 import br.com.watch.screenmatch.service.ConsumoApi;
+import br.com.watch.screenmatch.service.ConverteDados;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -17,11 +19,11 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		ConsumoApi consumoApi = new ConsumoApi();
 		
-		var json = consumoApi.obterDados("http://www.omdbapi.com/?t=gilmore+girls&Season=1&apikey=9c30019");
+		var json = consumoApi.obterDados("http://www.omdbapi.com/?t=gilmore+girls&apikey=9c30019");
 		System.out.println(json);
 		
-
-		
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
 	}
-
 }
