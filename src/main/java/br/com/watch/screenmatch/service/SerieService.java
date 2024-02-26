a406c27c7f4c38d7de8d1fc3eb2f429b0a7dc022
Felipe Gabriel <felipegabrielodss@gmail.com>
2024-02-26 11:03:13 -0300

    Retornando os detalhes da série

--- src/main/java/br/com/watch/screenmatch/service/SerieService.java
+++ src/main/java/br/com/watch/screenmatch/service/SerieService.java
@@ -1,6 +1,7 @@
 package br.com.watch.screenmatch.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,5 +33,14 @@ public class SerieService {
 	
 	public List<SerieDTO> obterLancamentos() {
 		return converteDados(repositorio.encontrarEpisodiosMaisRecentes());
+	}
+
+	public SerieDTO obterPorId(Long id) {
+		Optional<Serie> serie = repositorio.findById(id);
+		if(serie.isPresent()) {
+			Serie s = serie.get();
+			return new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse());
+		} 
+		return null;	
 	}
 }
