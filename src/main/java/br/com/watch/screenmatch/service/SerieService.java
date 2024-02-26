a14804c24dc58e1ad26fb0279652138c42d07412
Felipe Gabriel <felipegabrielodss@gmail.com>
2024-02-25 23:58:23 -0300

    Exibindo as top séries do banco

--- src/main/java/br/com/watch/screenmatch/service/SerieService.java
+++ src/main/java/br/com/watch/screenmatch/service/SerieService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.watch.screenmatch.dto.SerieDTO;
+import br.com.watch.screenmatch.model.Serie;
 import br.com.watch.screenmatch.repository.SerieRepository;
 
 @Service
@@ -16,8 +17,15 @@ public class SerieService {
 	private SerieRepository repositorio;
 	
 	public List<SerieDTO> obterTodasAsSeries() {
-		return repositorio.findAll()
-				.stream()
+		return converteDados(repositorio.findAll());
+	}
+
+	public List<SerieDTO> obterTop5Series() {
+		return converteDados(repositorio.findTop5ByOrderByAvaliacaoDesc());
+	}
+	
+	private List<SerieDTO> converteDados(List<Serie> series) {
+		return series.stream()
 				.map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
 				.collect(Collectors.toList());
 	}
